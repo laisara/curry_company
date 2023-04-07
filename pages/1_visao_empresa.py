e9be1624a1c9c99e211ae8dfeb9991c19137191d
laisara <109490601+laisara@users.noreply.github.com>
2023-04-07 20:22:19 -0300

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -169,7 +169,6 @@ df1 = clean_code(df_raw)
 
 st.header('Marketplace - Visão Cliente')
 
-#image_path = 'C:/Users/Lais/repos/ftc_programacao_python/logo.png'
 image = Image.open("images/logo.png")
 st.sidebar.image( image, width=120 )
 
